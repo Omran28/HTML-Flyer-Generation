7caaa34a8c9b173cc6e4296205b63769fac1a1c9
Omran28 <hoodaomran@hotmail.com>
2025-11-03 21:27:18 +0200

    Updating gemini

--- core/config.py
+++ core/config.py
@@ -7,8 +7,7 @@ load_dotenv()
 
 "API Keys"
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "")
-GEMINI_MODEL = os.getenv("GEMINI_MODEL", "gemini-2.0-flash")
-
+GEMINI_MODEL = os.getenv("GEMINI_MODEL", "models/gemini-2.5-pro")
 
 # Ensure the main API key is available for usage
 if GEMINI_API_KEY:
